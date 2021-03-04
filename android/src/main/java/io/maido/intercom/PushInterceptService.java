32637530ffcbe6f99d2d8501cb77561ba47030f3
daveols <10344370+daveols@users.noreply.github.com>
2021-03-04 23:02:28 +1100

    propagate non intercom fcm messages

--- android/src/main/java/io/maido/intercom/PushInterceptService.java
+++ android/src/main/java/io/maido/intercom/PushInterceptService.java
@@ -1,11 +1,15 @@
 package io.maido.intercom;
 import com.google.firebase.messaging.*;
 import io.intercom.android.sdk.push.IntercomPushClient;
+import android.content.Intent;
 import android.util.Log;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import java.util.Map;
 
 public class PushInterceptService extends FirebaseMessagingService {
     private static final String TAG = "PushInterceptService";
+    private static final String ACTION_REMOTE_MESSAGE = "io.flutter.plugins.firebasemessaging.NOTIFICATION";
+    private static final String EXTRA_REMOTE_MESSAGE = "notification";
     private final IntercomPushClient intercomPushClient = new IntercomPushClient();
 
     @Override
@@ -20,6 +24,9 @@ public class PushInterceptService extends FirebaseMessagingService {
             intercomPushClient.handlePush(getApplication(), message);
         } else {
             Log.d(TAG, "Push message received, not for Intercom");
+            Intent intent = new Intent(ACTION_REMOTE_MESSAGE);
+            intent.putExtra(EXTRA_REMOTE_MESSAGE, remoteMessage);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
         }
     }
 }
